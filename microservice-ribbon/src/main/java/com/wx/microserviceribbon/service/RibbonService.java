b00b7e2186ab0654dcf2380d28b04d8ce8b280a0
JCW <jimmywei19@163.com>
2018-10-08 22:50:11 +0800

    springcloudtest

--- microservice-ribbon/src/main/java/com/wx/microserviceribbon/service/RibbonService.java
+++ microservice-ribbon/src/main/java/com/wx/microserviceribbon/service/RibbonService.java
@@ -1,5 +1,6 @@
 package com.wx.microserviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.wx.microserviceribbon.pojo.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,12 @@ public class RibbonService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public Product getByName(String name) {
         return restTemplate.getForObject("http://micro-provider/product/getByName?productName=" + name, Product.class);
+    }
+
+    public Product hiError(String name) {
+        return new Product();
     }
 }
--- selfservice-use/src/main/java/com/wx/selfservice/SelfServiceApplication.java
+++ selfservice-use/src/main/java/com/wx/selfservice/SelfServiceApplication.java
@@ -1,9 +1,12 @@
 package com.wx.selfservice;
 
+import com.wx.microserviceribbon.service.FeignService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(clients = FeignService.class)
 public class SelfServiceApplication {
 
     public static void main(String[] args) {
