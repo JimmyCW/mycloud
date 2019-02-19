e444053db7bd116c99e3e77f30aa3a5482e04983
JCW <jimmywei19@163.com>
2019-02-19 10:20:41 +0800

    config

--- microservice-ribbon/src/main/java/com/wx/microserviceribbon/service/FeignService.java
+++ microservice-ribbon/src/main/java/com/wx/microserviceribbon/service/FeignService.java
@@ -3,7 +3,6 @@ package com.wx.microserviceribbon.service;
 import com.wx.microserviceribbon.pojo.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "micro-provider/product")
