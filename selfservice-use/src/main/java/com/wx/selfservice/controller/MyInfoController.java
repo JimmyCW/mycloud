e14f2f99157bdc4c12115dcd105f9fa7d3ab6932
JCW <jimmywei19@163.com>
2019-02-19 10:14:12 +0800

    config

--- selfservice-use/src/main/java/com/wx/selfservice/controller/MyInfoController.java
+++ selfservice-use/src/main/java/com/wx/selfservice/controller/MyInfoController.java
@@ -1,20 +1,31 @@
 package com.wx.selfservice.controller;
 
+import com.wx.microserviceribbon.service.FeignService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/myInfo")
 public class MyInfoController {
 
-    @Value("${self.my}")
-    private String my;
+    @Value("${spring.application.name}")
+    private String name;
 
-    @GetMapping("")
+    @Autowired
+    FeignService feignService;
+
+    @GetMapping("/product")
+    public Object product(@RequestParam String productName) {
+        return feignService.getByProductName(productName);
+    }
+
+    @GetMapping("/info")
     public String getInfo() {
-        return my;
+        return name;
     }
 
 }
