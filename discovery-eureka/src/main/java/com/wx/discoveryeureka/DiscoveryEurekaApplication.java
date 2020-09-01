a06fb55349e07c8f7c172fa1ca2f6237b6c1f984
weixing <jimmywei19@163.com>
2020-09-01 09:28:03 +0800

    iii

--- discovery-eureka/src/main/java/com/wx/discoveryeureka/DiscoveryEurekaApplication.java
+++ discovery-eureka/src/main/java/com/wx/discoveryeureka/DiscoveryEurekaApplication.java
@@ -4,15 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer
-@ComponentScan(basePackages = "com.wx.discoveryeureka")
 @ServletComponentScan
 public class DiscoveryEurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryEurekaApplication.class, args);
     }
+
 }
